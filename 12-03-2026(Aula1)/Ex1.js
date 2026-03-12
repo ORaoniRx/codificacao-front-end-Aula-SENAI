744ddaea99be8bd465b00f3544070e6e678c5385
ORaoniRx <138318685+ORaoniRx@users.noreply.github.com>
2026-03-12 16:01:54 -0300

    Exercicío 1 Raoni

---
+++ 12-03-2026(Aula1)/Ex1.js
@@ -0,0 +1,47 @@
+// Soma
+
+let num1 = 2
+let num2 = 2
+let resSoma = num1 + num2
+
+console.log(resSoma)
+
+// Subtração
+
+let num3 = 2
+let num4 = 6
+let resSub = num1 - num2
+
+console.log(resSub)   
+
+// Multiplicação
+
+let num5 = 2
+let num6 = 6
+let resMult = num1 * num2
+
+console.log(resMult)
+
+// Divisão
+
+let num7 = 12
+let num8 = 6
+let resDiv = num1 / num2
+
+console.log(resDiv)
+
+// Resto da Divisão
+
+let num9 = 10
+let num10 = 8
+let resRDIV = num1 % num2
+
+console.log(resRDIV) 
+
+// Exponenciação
+
+let num11 = 2
+let num12 = 6
+let resExp = num1 ** num2
+
+console.log(resExp)    
